70f3641f1aa7d982ac045a86bdbe7dce738ffa35
DESKTOP-5NEC0SV\thuongdh <hoaithuong.uit@gmail.com>
2017-08-11 14:38:21 +0700

    no message

--- deport/config/routes.rb
+++ deport/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   get 'store/index'
-
+  get 'product/index'
+  get 'line_items/index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
