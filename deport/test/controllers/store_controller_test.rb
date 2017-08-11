af3a60d0841087f0ad5d08153f705ee586285522
DESKTOP-5NEC0SV\thuongdh <hoaithuong.uit@gmail.com>
2017-08-11 13:56:21 +0700

    no message

--- deport/test/controllers/store_controller_test.rb
+++ deport/test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select '#colunms #side a',minimum: 4
+    assert_select '#main .entry',3
+    assert_select 'h3', 'programing ruby 1.9'
+    assert_select '.price',  /\$[,\d]+\.\d\d/
   end
 
 end
