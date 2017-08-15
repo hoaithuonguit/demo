348ebc86c1903681d124bfef37dfdde46a1d8848
DESKTOP-5NEC0SV\thuongdh <hoaithuong.uit@gmail.com>
2017-08-15 09:33:06 +0700

    no message

--- deport/db/schema.rb
+++ deport/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811092239) do
+ActiveRecord::Schema.define(version: 20170811093317) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
