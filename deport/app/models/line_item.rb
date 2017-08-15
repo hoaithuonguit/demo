00ceefc8806649038bfb4ffd8c46250aa234f8c2
DESKTOP-5NEC0SV\thuongdh <hoaithuong.uit@gmail.com>
2017-08-15 11:58:59 +0700

    no message

--- deport/app/models/line_item.rb
+++ deport/app/models/line_item.rb
@@ -1,4 +1,12 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    if self.product.price && self.quantity
+      self.product.price * self.quantity
+    else
+      "0.0".to_d
+    end
+  end
 end
