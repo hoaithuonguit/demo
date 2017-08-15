66dc27d60a7dadf776f30bed19738cc53fadf867
DESKTOP-5NEC0SV\thuongdh <hoaithuong.uit@gmail.com>
2017-08-15 17:42:18 +0700

    no message

--- deport/app/controllers/line_items_controller.rb
+++ deport/app/controllers/line_items_controller.rb
@@ -34,7 +34,7 @@ class LineItemsController < ApplicationController
         format.html {redirect_to store_index_url}
         format.js {@current_item = @line_item}
         format.json {render :show,
-                            status: :created, location: @line_item}
+              status: :created, location: @line_item}
       else
         format.html {render :new}
         format.json {render json: @line_item.errors,
