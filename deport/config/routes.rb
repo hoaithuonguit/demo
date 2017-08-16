60f4edb0ba1fd45ab60bf28d056b0748f5d3a886
DESKTOP-5NEC0SV\thuongdh <hoaithuong.uit@gmail.com>
2017-08-16 11:42:45 +0700

    add order

--- deport/config/routes.rb
+++ deport/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
+  resource :store
   get 'store/index'
   get 'product/create'
+  #root to: 'store#index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
