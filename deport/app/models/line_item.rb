44fd37d15fb76873e0e5b4bda650ddcb64759f68
DESKTOP-5NEC0SV\thuongdh <hoaithuong.uit@gmail.com>
2017-08-17 17:44:03 +0700

    no message

--- deport/app/models/line_item.rb
+++ deport/app/models/line_item.rb
@@ -4,10 +4,11 @@ class LineItem < ApplicationRecord
   belongs_to :cart
 
   def total_price
-    if self.product.price && self.quantity
-      self.product.price * self.quantity
-    else
-      "0.0".to_d
-    end
+    # if self.product.price && self.quantity
+    #   self.product.price * self.quantity
+    # else
+    #   "0.0".to_d
+    # end
+    product.price * quantity
   end
 end
